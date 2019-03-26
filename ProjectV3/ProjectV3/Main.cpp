b213972c270fe82b6be04f8846d3c07035b36e07
Mao Yupeng <yupengmao2-c@ad.cityu.edu.hk>
2019-03-26 10:29:06 +0800

    main.cpp 做了一点

--- ProjectV3/ProjectV3/Main.cpp
+++ ProjectV3/ProjectV3/Main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+bool login(string ID, string password) 
+{
+	if (true)
+	{
+
+	}
+}
+
+
 int main() 
 {
-	//code here
+	string userID, password;
+	cout << "Welcome to camp equipment loan system!" << endl;
+	cout << "Please enter your ID: ";
+	cin >> userID;
+	cout << "Please enter your password: ";
+	cin >> password;
+	if (login(userID, password))
+	{
+
+	}
+	else
+	{
+
+	}
 }
