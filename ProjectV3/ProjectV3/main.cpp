1b3a8092318a3ecb0ed8ec48d4407f256b8ff630
BRO-Z <44611354+BRO-Z@users.noreply.github.com>
2019-03-27 23:41:00 +0800

    一点点东西

---
+++ ProjectV3/ProjectV3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Lantern.h"
+#include "Stove.h"
+#include "Tent.h"
+
+using namespace std;
+
+int main() {
+	/*
+	
+	³ô
+	
+	Éµ
+	
+	±Æ
+	
+	×Ô
+	
+	¼º
+	
+	×ö
+	
+	*/
+}
