a1481663f28be71908e66bd7ad849122cc25d7b0
Mao Yupeng <yupengmao2-c@ad.cityu.edu.hk>
2019-04-01 20:54:53 +0800

    改动复原 诸位操作之前务必先fetch,pull

--- ProjectV3/ProjectV3/FileHandler.cpp
+++ ProjectV3/ProjectV3/FileHandler.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-//do what you want
+class fileHandler 
+{
+private:
+	string *reg = new string;
+public:
+	string *fileRead(string fileName, int line) 
+	{
+		istream input(fileName);
+		for (int i = 0; i < line; i++)
+		{
+			getline(input, reg);
+		}
+		input.close();
+		return reg;
+	}
+	void fileWrite() 
+	{
+
+
+	}
+};
