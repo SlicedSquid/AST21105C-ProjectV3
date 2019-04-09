7638e6c7a1bd370ccd6f1cf43409fd51b49e6ea7
LIN Guangzheng <guangzlin2-c@my.cityu.edu.hk>
2019-04-10 01:00:16 +0800

    [Zan] Sucking "getMainMenu" in main

--- ProjectV3/ProjectV3/main.cpp
+++ ProjectV3/ProjectV3/main.cpp
@@ -7,17 +7,24 @@ using namespace std;
 int getMainMenu()
 {
 	int choice;
-	cout << "------------------------------------" << endl;
-	cout << "1) Display available camp equipments" << endl;
-	cout << "2) Display loan record" << endl;
-	cout << "3) Make equipment loan" << endl;
-	cout << "4) Return equipment" << endl;
-	cout << "5) Exit" << endl;
-	cout << "------------------------------------" << endl;
-	cout << "Please enter your choice: ";
-	cin >> choice;
+	bool run;
+	do {
+		cout << "------------------------------------" << endl;
+		cout << "1) Display available camp equipments" << endl;
+		cout << "2) Display loan record" << endl;
+		cout << "3) Make equipment loan" << endl;
+		cout << "4) Return equipment" << endl;
+		cout << "5) Exit" << endl;
+		cout << "------------------------------------" << endl;
+		cout << "Please enter your choice: ";
+		cin >> choice;
+		cout << "------------------------------------" << endl;
+		// Run the program when the "choice" is correct
+		run = true;
+		if (choice < 1 || choice > 5)
+			run = false;
+	} while (!run);
 	return choice;
-	cout << "------------------------------------" << endl;
 }
 
 int main()
@@ -29,13 +36,13 @@ int main()
 	cout << "Welcome to camp equipments borrow system!" << endl;
 	while ()
 	{
-		cout << "Please enter your userID: ";
+		cout << "Please enter user ID: ";
 		cin >> userID;
-		cout << "Please enter your password: ";
+		cout << "Please enter password: ";
 		cin >> password;
 		if (administrator.login(userID, password))
 		{
-			cout << "Login successed!" << endl;
+			cout << "You're successfully logged in." << endl;
 			users a(userID);
 			switch (getMainMenu())
 			{
@@ -52,14 +59,14 @@ int main()
 				administrator.returnEquipment(a);
 				break;
 			case 5:
-				cout << "Thanks for using." << endl;
+				cout << "Thanks for using!" << endl;
 				break;
 			}
 			break;
 		}
 		else
 		{
-			cout >> "Invalid user ID or password. Please try again.";
+			cout >> "Your account or password was entered incorrectly. Please try again.";
 		}
 	}
 	system("pause");
