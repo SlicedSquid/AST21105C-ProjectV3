cc38352c254645bd2cb5f7973483e901bb7ab980
AnthonyW <44766609+a88183119q@users.noreply.github.com>
2019-04-12 15:05:14 +0800

    Add files via upload
    改了一些

--- ProjectV3/ProjectV3/admin.cpp
+++ ProjectV3/ProjectV3/admin.cpp
@@ -11,12 +11,41 @@ void admin::displayAvailableEquipment() const
 
 void admin::displayLoanRecord(users a) const
 {
+
+	ifstream myfile;
+	myfile.open("user.txt");
+	string a;
+
+	if (myfile.is_open())
+	{
+		while (getline(myfile, a))
+		{
+			cout << a << endl;
+
+		}
+		cout << endl;
+		myfile.close();
+	}
+
 }
 
 void admin::makeLoan(users a) const
 {
+	ifstream file;
+	file.open("camp_equipment.txt");
+	string b;
+	if (file.is_open())
+	{
+		while (getline(file, b))
+		{
+			cout << b << endl;
+		}
+		file.close();
+	}
 }
 
 void admin::returnEquipment(users a) const
 {
 }
+
+
--- ProjectV3/ProjectV3/admin.h
+++ ProjectV3/ProjectV3/admin.h
@@ -2,16 +2,18 @@
 #include <iostream>
 #ifndef ADMIN_H 
 #define ADMIN_H
-class admin 
+class admin
 {
 private:
-	
+	string a;
+	string b;
+
 public:
 	void displayAvailableEquipment() const;
 	void displayLoanRecord(users a) const;
+	void displaycampofequipment(user a) const;
 	void makeLoan(users a) const;
 	void returnEquipment(users a) const;
 };
 
 #endif // !ADMIN_H 
-
