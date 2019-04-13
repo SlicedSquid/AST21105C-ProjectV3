8fca369a823190eb6e68098f45092734dc876d19
X1A0Jc <44594895+X1A0Jc@users.noreply.github.com>
2019-04-13 14:03:16 +0800

    do nothing but add comments

--- ProjectV3/ProjectV3/Users.cpp
+++ ProjectV3/ProjectV3/Users.cpp
@@ -4,201 +4,234 @@
 #include "FileHandler.h"
 using namespace std;
 
+//get name
 string User::getName() {
-	return Name;
+    return Name;
 }
-
+//get id
 string User::getId() {
-	return Id;
+    return Id;
 }
-
+//get section
 string User::getSection() {
-	return Section;
+    return Section;
 }
 
+//get address
 string User::getAddress() {
-	return Address;
+    return Address;
 }
-
+//set id
 void User::setId(string id)
 {
-	this->Id = id;
+    this->Id = id;
 }
 
+//set password
 void User::setPassword(string password)
 {
-	this->password = password;
+    this->password = password;
 }
-
+//check user can loan or not
 void User::setLoanStatus(bool status)
 {
-	onLoan = status;
+    onLoan = status;
 }
-
+//set how many items that user can loan
 void User::setLoanNumber(int number)
 {
-	LoanNumber -= number;
+    LoanNumber -= number;
 }
 
-
+//get dateofbirth
 int User::getDateOfBirth() {
-	return DateOfBirth;
+    return DateOfBirth;
 }
 
+//defalut constructor to prevent error
 User::User() {}
 
+//prepared for the inheritance
 User::User(string _Id, string _Name, string _Section, string _Address, int _DateOfBirth) {
-	Id = _Id;
-	Name = _Name;
-	Section = _Section;
-	Address = _Address;
-	DateOfBirth = _DateOfBirth;
+    Id = _Id;
+    Name = _Name;
+    Section = _Section;
+    Address = _Address;
+    DateOfBirth = _DateOfBirth;
 }
 
+//sub class Scout inheritance from the class user
 Scout::Scout(string Id, string Name, string Section, string Address, int DateOfBirth, string  _Rank) {
-	Rank = _Rank;
-	this->Id = Id;
-	this->Name = Name;
-	this->Section = Section;
-	this->Address = Address;
-	this->DateOfBirth = DateOfBirth;
-	if (_Rank == "Assistant Patrol Leader" || _Rank == "Patrol Leader") {
-		LoanNumber = 3;
-	}
-	else
-	{
-		//only member can loan 1 item
-		LoanNumber = 1;
-	}
+    Rank = _Rank;
+    this->Id = Id;
+    this->Name = Name;
+    this->Section = Section;
+    this->Address = Address;
+    this->DateOfBirth = DateOfBirth;
+    
+    //for Assistant Patrol Leader and Patrol Leader, they can loan up to 3 items
+    if (_Rank == "Assistant Patrol Leader" || _Rank == "Patrol Leader") {
+        LoanNumber = 3;
+    }
+    else
+    {
+        //member can only loan 1 item
+        LoanNumber = 1;
+    }
 }
 
+//defalut constructor to prevent error
 Scout::Scout(){}
 
+//check scout can loan or not
 bool Scout::getStatus()
 {
-	return onLoan;
+    return onLoan;
 }
 
+//if Scout borrow one item, then loannumber - 1(loannumber down to 0)
 void Scout::borrowItem()
 {
-	LoanNumber--;
-	onLoan = true;
+    LoanNumber--;
+    onLoan = true;
 }
 
+//if Scout return one item, then loannumber + 1(for loannumber up to 3)
 void Scout::returnItem()
 {
-	LoanNumber++;
-	if (LoanNumber == 3 && (rank == "Assistant Patrol Leader"|| rank == "Patrol Leader"))
-	{
-		onLoan = false;
-	}
-	else
-	{
-		if (LoanNumber == 1 && rankrank != "Assistant Patrol Leader" && rank != "Patrol Leader")
-		{
-			onLoan = false;
-		}
-	}
+    LoanNumber++;
+    //for Assistant Patrol Leader and Patrol Leader the loannumber up to 3
+    if (LoanNumber == 3 && (rank == "Assistant Patrol Leader"|| rank == "Patrol Leader"))
+    {
+        //set they did not borrow anything
+        onLoan = false;
+    }
+    else
+    {
+        //for member the loannumber up to 1
+        if (LoanNumber == 1 && rankrank != "Assistant Patrol Leader" && rank != "Patrol Leader")
+        {
+            //set they did not borrow anything
+            onLoan = false;
+        }
+    }
 }
 
+//sub class VentureScout inheritance from the class user
 VentureScout::VentureScout(string Id, string Name, string Section, string Address, int DateOfBirth) {
-	this->Id = Id;
-	this->Name = Name;
-	this->Section = Section;
-	this->Address = Address;
-	this->DateOfBirth = DateOfBirth;
-	LoanNumber = 3;
-	cout << "Loan Number is " << LoanNumber << endl;
+    this->Id = Id;
+    this->Name = Name;
+    this->Section = Section;
+    this->Address = Address;
+    this->DateOfBirth = DateOfBirth;
+    LoanNumber = 3;
 }
 
+//defalut constructor to prevent error
 VentureScout::VentureScout(){}
 
+//check VentureScout can loan or not
 bool VentureScout::getStatus()
 {
-	return onLoan;
+    return onLoan;
 }
-
+//if VentureScout borrow one item, then loannumber - 1(loannumber down to 0)
 void VentureScout::borrowItem()
 {
-	LoanNumber--;
-	onLoan = true;
+    LoanNumber--;
+    onLoan = true;
 }
 
+//if VentureScout return one item, then loannumber + 1(for loannumber up to 3)
 void VentureScout::returnItem()
 {
-	LoanNumber++;
-	if (LoanNumber == 3)
-	{
-		onLoan = false;
-	}
+    LoanNumber++;
+    //for VentureScout loannumber up to 3
+    if (LoanNumber == 3)
+    {
+        //set they did not borrow anything
+        onLoan = false;
+    }
 }
-
+//sub class RoverScout inheritance from the class user
 RoverScout::RoverScout(string Id, string Name, string Section, string Address, int DateOfBirth) {
-	this->Id = Id;
-	this->Name = Name;
-	this->Section = Section;
-	this->Address = Address;
-	this->DateOfBirth = DateOfBirth;
-	LoanNumber = 5;
+    this->Id = Id;
+    this->Name = Name;
+    this->Section = Section;
+    this->Address = Address;
+    this->DateOfBirth = DateOfBirth;
+    LoanNumber = 5;
 }
 
-RoverScout::RoverScout()
-{
-}
+//defalut constructor to prevent error
+RoverScout::RoverScout(){}
 
+//check RoverScout can loan or not
 bool RoverScout::getStatus()
 {
-	if (LoanNumber == 0)
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
+    
+    if (LoanNumber == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
-
+//if RoverScout borrow one item, then loannumber - 1(loannumber down to 0)
 void RoverScout::borrowItem()
 {
-	LoanNumber--;
-	onLoan = true;
+    LoanNumber--;
+    //set they borrow something
+    onLoan = true;
 }
-
+//if VentureScout return one item, then loannumber + 1(for loannumber up to 5)
 void RoverScout::returnItem()
 {
-	LoanNumber++;
-	if (LoanNumber == 5)
-	{
-		onLoan = false;
-	}
+    LoanNumber++;
+    if (LoanNumber == 5)
+    {
+          //set they did not borrow anything
+        onLoan = false;
+    }
 }
 
+//sub class Scouter inheritance from the class user
 Scouter::Scouter(string Id, string Name, string Section, string Address, int DateOfBirth, string  _Rank) {
-	this->Id = Id;
-	this->Name = Name;
-	this->Section = Section;
-	this->Address = Address;
-	this->DateOfBirth = DateOfBirth;
-	LoanNumber = 5;
+    this->Id = Id;
+    this->Name = Name;
+    this->Section = Section;
+    this->Address = Address;
+    this->DateOfBirth = DateOfBirth;
+    LoanNumber = 5;
 }
 
-Scouter::Scouter()
-{
-}
-
+Scouter::Scouter(){}
+//check Scouter can loan or not
 bool Scouter::getStatus()
 {
-	if (LoanNumber == 0)
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
+    if (LoanNumber == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
-
+//if Scouter borrow one item, then loannumber - 1(loannumber down to 0)
 void Scouter::borrowItem()
 {
-	LoanNumber--;
+    LoanNumber--;
+}
+
+//if VentureScout return one item, then loannumber + 1(for loannumber up to 5)
+void Scouter::returnItem()
+{
+    LoanNumber++;
+    if (LoanNumber == 5)
+    {
+        onLoan = false;
+    }
 }
